6a84d32752553fb27ad703bbbe901e43d6a641d5
Geto1-creator <84434642+Geto1-creator@users.noreply.github.com>
2024-03-04 22:33:41 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LoginButton from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
@@ -11,22 +12,27 @@ const font = Poppins({
 });
 
 export default function Home() {
-  console.log("Where am i ");
+  // console.log("Where am i ");
   return (
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-500 to-blue-800">
-      <div className="space-y-6">
+      <div className="space-y-6 flex flex-col items-center">
         <h1
           className={cn(
             "text-6xl font-semibold text-white drop-shadow-md",
             font.className
-          )}
-        >
-          Auth{" "}
+          )}>
+          🔐Auth{" "}
         </h1>
-        <p className="text-white text-lg">A simple authentications service</p>
+        <p className="text-white text-lg text-center">
+          A simple authentications service
+        </p>
 
         <div>
-          <Button variant="secondary" size="lg">Sign In</Button>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign In
+            </Button>
+          </LoginButton>
         </div>
       </div>
     </main>
---
+++ components/auth/login-button.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => {
+  const router = useRouter();
+  const onClick = () => {
+    router.push("/auth/login");
+    console.log("Login button clicked");
+  };
+  if (mode == "modal") {
+    return <span>TODO: Implement modal</span>;
+  }
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {" "}
+      {children}
+    </span>
+  );
+};
+
+export default LoginButton;
